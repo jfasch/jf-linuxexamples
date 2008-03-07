c510a244fad4bf35ab3fb39e3bbfb478a76d2c3f
jfasch <jfasch@8b113dd5-8918-0410-b1dd-ae3631fd8999>
2008-03-07 15:02:55 +0000

    git-svn-id: https://confix.svn.sourceforge.net/svnroot/confix/unix-tools@540 8b113dd5-8918-0410-b1dd-ae3631fd8999

--- trunk/pthread/future.h
+++ trunk/pthread/future.h
@@ -16,7 +16,6 @@
 // License along with this library; if not, write to the Free Software
 // Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
 // USA
-// (C) Atronic -*- C++ -*-
 
 #ifndef HAVE_JFLINUX_PTHREAD_FUTURE_H
 #define HAVE_JFLINUX_PTHREAD_FUTURE_H
